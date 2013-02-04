6e2634b6bcc6e7f2b49bdedd01b0e84a5bd7bd23
Takumi Akiyama <t.akiym@gmail.com>
2013-02-04 13:35:36 +0900

    fix pwm gain

--- h/motor.h
+++ h/motor.h
@@ -2,18 +2,18 @@
 #define __MOTOR_H
 
 #define PWM_BASE 21000
-#define PWM_L_BASE 20860
-#define PWM_R_BASE 20860
-#define PWM_F_BASE PWM_BASE
-#define PWM_B_BASE PWM_BASE
+#define PWM_L_BASE 20930
+#define PWM_R_BASE 20870
+#define PWM_F_BASE 21060
+#define PWM_B_BASE 21060
 
 #define PWM_MAX 28000
 #define PWM_MIN 20000
 #define PWM_STOP 18000
 
-#define PWM_THROTTLE 8
-#define PWM_HANDLE 2
-#define PWM_POSTURE 16
+#define PWM_THROTTLE 14
+#define PWM_HANDLE 1
+#define PWM_POSTURE 12
 #define PWM_ROTATE 4
 
 #define ACCE_KP 20
