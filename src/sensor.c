4a91698b1daa97a698bd25398648450bf3ffd8e4
Takumi Akiyama <t.akiym@gmail.com>
2013-02-14 15:02:22 +0900

    oops oops

--- src/sensor.c
+++ src/sensor.c
@@ -31,10 +31,10 @@ void setAngle()
 
 void setAnglePd()
 {
-    anglePd[0] = angle[0] * (rx.buf[RX_ANGLE_KP] / 10)
-        + (angleBefore[0] - angle[0]) * (rx.buf[RX_ANGLE_KD] / 10);
-    anglePd[1] = angle[1] * (rx.buf[RX_ANGLE_KP] / 10)
-        + (angleBefore[1] - angle[1]) * (rx.buf[RX_ANGLE_KD] / 10);
+    anglePd[0] = angle[0] * (rx.buf[RX_ANGLE_KP] / 10.0)
+        + (angleBefore[0] - angle[0]) * (rx.buf[RX_ANGLE_KD] / 10.0);
+    anglePd[1] = angle[1] * (rx.buf[RX_ANGLE_KP] / 10.0)
+        + (angleBefore[1] - angle[1]) * (rx.buf[RX_ANGLE_KD] / 10.0);
 
     anglePd[0] = ANGLE_LIMIT(anglePd[0]);
     anglePd[1] = ANGLE_LIMIT(anglePd[1]);
@@ -42,12 +42,12 @@ void setAnglePd()
 
 void setGyroPd()
 {
-    gyroPd[0] = gyro[0] * (rx.buf[RX_GYRO_KP] / 10)
-        + (gyroBefore[0] - gyro[0]) * (rx.buf[RX_GYRO_KD] / 10);
-    gyroPd[1] = gyro[1] * (rx.buf[RX_GYRO_KP] / 10)
-        + (gyroBefore[1] - gyro[1]) * (rx.buf[RX_GYRO_KD] / 10);
-    gyroPd[2] = gyro[2] * (rx.buf[RX_GYRO_KP] / 10)
-        + (gyroBefore[2] - gyro[2]) * (rx.buf[RX_GYRO_KD] / 10);
+    gyroPd[0] = gyro[0] * (rx.buf[RX_GYRO_KP] / 10.0)
+        + (gyroBefore[0] - gyro[0]) * (rx.buf[RX_GYRO_KD] / 10.0);
+    gyroPd[1] = gyro[1] * (rx.buf[RX_GYRO_KP] / 10.0)
+        + (gyroBefore[1] - gyro[1]) * (rx.buf[RX_GYRO_KD] / 10.0);
+    gyroPd[2] = gyro[2] * (rx.buf[RX_GYRO_KP] / 10.0)
+        + (gyroBefore[2] - gyro[2]) * (rx.buf[RX_GYRO_KD] / 10.0);
 
     gyroPd[0] = GYRO_LIMIT(gyroPd[0]);
     gyroPd[1] = GYRO_LIMIT(gyroPd[1]);
